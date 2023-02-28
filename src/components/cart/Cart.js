8643ba312714607fa2088d068c734cede167c01f
Vuchkoo <filipvuk97@gmail.com>
2023-02-28 15:51:49 +0100

    feat: add to cart functionality

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,37 +1,89 @@
-import { Button, Drawer } from "@mantine/core";
-import React, { useState } from "react";
+import {
+  Button,
+  Flex,
+  Group,
+  Image,
+  Indicator,
+  ScrollArea,
+  Text,
+} from "@mantine/core";
+import { IconShoppingCart } from "@tabler/icons";
+import React from "react";
 
-const Cart = () => {
+const Cart = ({ cart }) => {
+  // console.log(cart);
   return (
     <>
-      <div className="cart-main">
-        <div className="cart-card">
-          <div className="cart-item">
-            <img alt="test" style={{ width: "100px", height: "150px" }} />
-            <div className="item-details">
-              <h3>Title</h3>
-              <p>Description</p>
-              <p>Quantity: ?</p>
-            </div>{" "}
-          </div>
-          <div className="item-quantity">
-            <Button variant="light" compact color="dark" size="xs">
-              x
-            </Button>
-            <h3>$$$</h3>
-            <Button>-</Button>
-            <Button>+</Button>
-          </div>
+      <Group position="center" pb={10}>
+        <Text>Cart</Text>
+        <Indicator
+          position="bottom-end"
+          label={cart.length}
+          size={14}
+          showZero={false}
+        >
+          <Flex justify="center" align="center">
+            <IconShoppingCart />
+          </Flex>
+        </Indicator>
+      </Group>
+      <ScrollArea
+        style={{ height: 500 }}
+        scrollbarSize={8}
+        scrollHideDelay={1500}
+      >
+        <div className="cart-main">
+          {cart.map((item) => {
+            return (
+              <div className="cart-card">
+                <div className="cart-item">
+                  <Image
+                    src={item.image}
+                    alt="item.title"
+                    radius="md"
+                    width={80}
+                    height={100}
+                  />
+                  <Flex
+                    direction="column"
+                    justify="center"
+                    className="item-details"
+                  >
+                    <Text>{item.title}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>Quantity: 1</Text>
+                  </Flex>
+                </div>
+                <div className="item-quantity">
+                  <Button variant="light" compact color="dark" size="xs">
+                    x
+                  </Button>
+                  <h3>${item.price.toFixed(2)}</h3>
+                  <Button>-</Button>
+                  <Button>+</Button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </div>
+      </ScrollArea>
       <div className="cart-footer">
         <div className="price">
           <h3>SUBTOTAL</h3>
           <div>
-            <h4>$</h4>
+            <h4>
+              $
+              {cart
+                .reduce((acc, cart) => {
+                  return cart.price + acc;
+                }, 0)
+                .toFixed(2)}
+            </h4>
           </div>
         </div>
-        <Button color="green">ORDER</Button>
+        <Button color="green" fullWidth>
+          ORDER
+        </Button>
       </div>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,182 +1,82 @@
 import Header from "../components/Header";
-import {
-  Grid,
-  Text,
-  Button,
-  Card,
-  Group,
-  Pagination,
-  Center,
-} from "@mantine/core";
+import { Grid, Text, Button, Card, Center, Flex, Image } from "@mantine/core";
 import Sidebar from "../components/Sidebar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios(`/product.json`)
+      .then((res) => {
+        setProducts(res.data.product);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleAddToCart = (e, item) => {
+    console.log(item);
+    setCart(item);
+    // const itExists = cart.some((cart) => {
+    //   return cart.id === item.id;
+    // });
+    // if (itExists) {
+    //   setCart(
+    //     cart?.map((cart) => {
+    //       if (cart.id === item.id) {
+    //         return { ...cart, quantity: cart.quantity + 1 };
+    //       }
+    //       return cart;
+    //     })
+    //   );
+    // } else {
+    //   return setCart([...cart, { ...item }]);
+    // }
+  };
+
+  console.log(cart);
+
   return (
     <div>
-      <Header />
+      <Header cart={cart} />
       <div className="grid">
         <Sidebar />
         <div className="product-grid">
           <Grid mt={40}>
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
-
-            <Card withBorder radius="md" className="product" m={5}>
-              <img alt="test" />
-              <Group position="apart" mt="md">
-                <div>
-                  <Text weight={500}>Test</Text>
-                  <Text size="xs" color="dimmed">
-                    Test
-                  </Text>
-                </div>
-              </Group>
-              <Text size="xl" weight={700}>
-                $168.00
-              </Text>
-              <Button>Add to cart</Button>
-            </Card>
+            {products?.map((item) => {
+              return (
+                <Card withBorder radius="md" className="product" m={5}>
+                  <Image
+                    src={item.image}
+                    alt="item.title"
+                    radius="md"
+                    width={150}
+                    height={150}
+                  />
+                  <Flex direction="column" justify="center" align="center">
+                    <Text mt={10} weight={500}>
+                      {item.title}
+                    </Text>
+                    <Text size="xs" color="dimmed">
+                      {item.description}
+                    </Text>
+                    <Text size="xl" weight={700}>
+                      ${item.price.toFixed(2)}
+                    </Text>
+                    <Button
+                      mt={10}
+                      onClick={() => setCart([...cart, { ...item }])}
+                    >
+                      Add to cart
+                    </Button>
+                  </Flex>
+                </Card>
+              );
+            })}
           </Grid>
           <Center mt={50}>
             <Button color="green">Load more</Button>
