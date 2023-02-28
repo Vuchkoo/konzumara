159e36fb8bf7a197a942bb455eb4a4a1cbfcc9d9
Vuchkoo <filipvuk97@gmail.com>
2023-02-28 12:00:44 +0100

    feat: added routes for admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import Categories from "./pages/admin/Categories";
+import Orders from "./pages/admin/Orders";
+import Products from "./pages/admin/Products";
 import { Error } from "./pages/Error";
 import Home from "./pages/Home";
 import UserDashboard from "./pages/user/UserDashboard";
@@ -12,6 +16,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="user/dashboard" element={<UserDashboard />} />
+            <Route path="admin" element={<AdminDashboard />} />
+            <Route path="admin/products" element={<Products />} />
+            <Route path="admin/categories" element={<Categories />} />
+            <Route path="admin/orders" element={<Orders />} />
             <Route path="*" element={<Error />} />
           </Routes>
         </BrowserRouter>
