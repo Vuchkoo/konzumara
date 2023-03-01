3ff533607facc54ae9bc0d3f0bbfc69db14353e4
Vuchkoo <filipvuk97@gmail.com>
2023-03-01 13:54:16 +0100

    feat: admin sign in page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { MantineProvider } from "@mantine/core";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminSignIn from "./pages/admin/AdminSignIn";
 import Categories from "./pages/admin/Categories";
 import Orders from "./pages/admin/Orders";
 import Products from "./pages/admin/Products";
 import { Error } from "./pages/Error";
 import Home from "./pages/Home";
-import UserDashboard from "./pages/user/UserDashboard";
+
 function App() {
   return (
     <div className="App">
@@ -15,8 +16,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="user/dashboard" element={<UserDashboard />} />
-            <Route path="admin" element={<AdminDashboard />} />
+            <Route path="admin" element={<AdminSignIn />} />
+            <Route path="admin/dashboard" element={<AdminDashboard />} />
             <Route path="admin/products" element={<Products />} />
             <Route path="admin/categories" element={<Categories />} />
             <Route path="admin/orders" element={<Orders />} />
---
+++ src/pages/admin/AdminSignIn.js
@@ -0,0 +1,74 @@
+import {
+  Box,
+  Button,
+  Center,
+  Group,
+  PasswordInput,
+  TextInput,
+} from "@mantine/core";
+import { IconAt } from "@tabler/icons";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AdminSignIn = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [data, setData] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios(`/user.json`)
+      .then((res) => {
+        setData(res.data.user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSignInSubmit = (e) => {
+    if (data[1].email === form.email && data[1].password === form.password) {
+      e.preventDefault();
+      navigate("dashboard");
+    } else {
+      e.preventDefault();
+      alert("Invalid email or password");
+    }
+  };
+
+  return (
+    <Center style={{ height: 600 }}>
+      <Box sx={{ maxWidth: 300 }} mx="auto">
+        <form onSubmit={onSignInSubmit}>
+          <TextInput
+            name="email"
+            label="Email"
+            onChange={handleChange}
+            rightSection={<IconAt size={14} color="gray" />}
+            placeholder="Email"
+          />
+
+          <PasswordInput
+            name="password"
+            mt="md"
+            label="Password"
+            onChange={handleChange}
+            placeholder="Password"
+          />
+
+          <Group position="center" mt="md">
+            <Button type="submit" uppercase>
+              Sign in
+            </Button>
+          </Group>
+        </form>
+      </Box>
+    </Center>
+  );
+};
+
+export default AdminSignIn;
