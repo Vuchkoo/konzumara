0c32b6155bae91739c5f469d4288b7156bc582bf
Vuchkoo <filipvuk97@gmail.com>
2023-03-06 09:14:56 +0100

    feat: connected sign in with supabase

--- src/components/forms/SignIn.js
+++ src/components/forms/SignIn.js
@@ -1,22 +1,41 @@
 import { Box, Button, Group, PasswordInput, TextInput } from "@mantine/core";
 import { IconAt } from "@tabler/icons";
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../../config/Supabase";
 
-const SignIn = ({ data, form, handleChange, setUser, setSignInOpened }) => {
-  const onSignInSubmit = (e) => {
-    if (data[0].email === form.email && data[0].password === form.password) {
-      e.preventDefault();
-      setUser(true);
-      setSignInOpened(false);
-    } else {
-      e.preventDefault();
+// const SignIn = ({ data, form, handleChange, setUser, setSignInOpened }) => {
+const SignIn = ({ form, handleChange, setSignInOpened, user, setUser }) => {
+  const { email, password } = form.values;
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    if (error) {
       alert("Invalid email or password");
+    } else {
+      setUser(data.user);
+      setSignInOpened(false);
+      console.log(data.user);
     }
   };
 
+  // const onSignInSubmit = (e) => {
+  //   if (data[0].email === form.email && data[0].password === form.password) {
+  //     e.preventDefault();
+  //     setUser(true);
+  //     setSignInOpened(false);
+  //   } else {
+  //     e.preventDefault();
+  //     alert("Invalid email or password");
+  //   }
+  // };
+
   return (
     <Box sx={{ maxWidth: 300 }} mx="auto">
-      <form onSubmit={onSignInSubmit}>
+      <form onSubmit={handleSignIn}>
         <TextInput
           name="email"
           label="Email"
