60c92ddfffd71edc1f1c99952cf79b9beff30e77
Vuchkoo <filipvuk97@gmail.com>
2023-03-06 17:22:42 +0100

    feat: fetched data from supabase

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,30 +1,63 @@
 import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
+import { supabase } from "../config/Supabase";
 
 export const Context = createContext(null);
 
 export const DataProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [products, setProducts] = useState(null);
+  const [categories, setCategories] = useState(null);
   const [loading, setLoading] = useState(true);
-  const value = { user, setUser, products, setProducts, loading, setLoading };
+  const value = {
+    user,
+    setUser,
+    products,
+    setProducts,
+    categories,
+    setCategories,
+    loading,
+    setLoading,
+  };
+
+  // useEffect(() => {
+  //   axios(`/product.json`)
+  //     .then((res) => {
+  //       setProducts(res.data.product);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  //   axios(`/user.json`)
+  //     .then((res) => {
+  //       setUser(res.data.user);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   useEffect(() => {
-    axios(`/product.json`)
-      .then((res) => {
-        setProducts(res.data.product);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    axios(`/user.json`)
-      .then((res) => {
-        setUser(res.data.user);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const getProducts = async () => {
+      const { data, error } = await supabase.from("products").select();
+      setProducts(data);
+      setLoading(false);
+    };
+
+    const getCategories = async () => {
+      const { data, error } = await supabase.from("categories").select();
+      setCategories(data);
+      setLoading(false);
+    };
+
+    if (loading) {
+      setProducts(getProducts());
+      setCategories(getCategories());
+      // setLoading(true);
+    }
   }, []);
 
-  return <Context.Provider value={value}>{children}</Context.Provider>;
+  return (
+    <Context.Provider value={value}>{!loading && children}</Context.Provider>
+  );
 };
