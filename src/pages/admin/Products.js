4dd042782dba5fc6d7e34caffe9dd1b09caedce3
Vuchkoo <filipvuk97@gmail.com>
2023-03-08 14:07:16 +0100

    feat: products pagination functionality

--- src/pages/admin/Products.js
+++ src/pages/admin/Products.js
@@ -2,7 +2,6 @@ import {
   ActionIcon,
   Button,
   Center,
-  Checkbox,
   Flex,
   Image,
   Modal,
@@ -13,15 +12,7 @@ import {
   TextInput,
   useMantineTheme,
 } from "@mantine/core";
-import {
-  IconCheck,
-  IconCheckbox,
-  IconChecklist,
-  IconChecks,
-  IconEdit,
-  IconSearch,
-  IconTrash,
-} from "@tabler/icons";
+import { IconCheck, IconEdit, IconSearch, IconTrash } from "@tabler/icons";
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { NavbarSimple } from "../../components/admin/Navbar";
@@ -31,10 +22,14 @@ import { Context } from "../../context/Context";
 
 const Products = () => {
   const [editOpened, setEditOpened] = useState(false);
-  const { products, setProducts, categories, setCategories } =
+  const { products, setProducts, categories, setCategories, user } =
     useContext(Context);
   const navigate = useNavigate();
   const theme = useMantineTheme();
+  const [page, setPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(8);
+
+  console.log(user);
 
   const {
     name,
@@ -83,6 +78,18 @@ const Products = () => {
     ]);
   };
 
+  const lastProduct = page * productsPerPage;
+  const firstProduct = lastProduct - productsPerPage;
+  const currentProducts = products.slice(firstProduct, lastProduct);
+
+  const handleChangePage = (prevPage) => {
+    page > prevPage
+      ? setPage((prevPage) => prevPage - 1)
+      : setPage((prevPage) => prevPage + 1);
+  };
+
+  // console.log(user);
+
   return (
     <div className="grid">
       <NavbarSimple />
@@ -99,110 +106,99 @@ const Products = () => {
         </Flex>
         <div className="products">
           <h2>Products</h2>
-          <ScrollArea
+          {/* <ScrollArea
             style={{ height: 500 }}
             scrollbarSize={8}
             scrollHideDelay={1500}
-          >
-            <Table>
-              <thead>
-                <tr>
-                  <th>Image</th>
-                  <th>Name</th>
-                  <th>Description</th>
-                  <th>Price</th>
-                  <th>Quantity</th>
-                  <th>Sale</th>
-                  <th>Sale Price</th>
-                  <th>Category ID</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products?.map((item) => {
-                  return (
-                    <tr key={item.id}>
-                      <td>
-                        <Image
-                          src={item.image}
-                          alt={item.title}
-                          radius="md"
-                          width={50}
-                          height={50}
-                        />
-                      </td>
-                      <td>
-                        <Text>{item.name}</Text>
-                      </td>
-                      <td>
-                        <Text>{item.description}</Text>
-                      </td>
-                      <td>
-                        <Text>${item.price.toFixed(2)}</Text>
-                      </td>
-                      <td>
-                        <Text>{item.quantity}</Text>
-                      </td>
-                      <td>
-                        <Text>
-                          {item.is_sale ? <IconCheck color="green" /> : null}
-                        </Text>
-                      </td>
-                      <td>
-                        <Text>
-                          {item.sale_price
-                            ? `$${item.sale_price.toFixed(2)}`
-                            : null}
-                        </Text>
-                      </td>
-                      <td>
-                        <Text>{item.category_id}</Text>
-                      </td>
-                      <td>
-                        <Flex>
-                          <ActionIcon
-                            color="yellow"
-                            onClick={() => {
-                              setEditOpened(true);
-                              console.log(item.id);
-                            }}
-                          >
-                            <IconEdit />
-                          </ActionIcon>
-                          <ActionIcon
-                            ml={10}
-                            color="red"
-                            onClick={(e) => handleRemoveProduct(e, item.id)}
-                          >
-                            <IconTrash />
-                          </ActionIcon>
-                        </Flex>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </ScrollArea>
+          > */}
+          <Table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Sale</th>
+                <th>Sale Price</th>
+                <th>Category ID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentProducts?.map((item) => {
+                return (
+                  <tr key={item.id}>
+                    <td>
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        radius="md"
+                        width={50}
+                        height={50}
+                      />
+                    </td>
+                    <td>
+                      <Text>{item.name}</Text>
+                    </td>
+                    <td>
+                      <Text>{item.description}</Text>
+                    </td>
+                    <td>
+                      <Text>${item.price.toFixed(2)}</Text>
+                    </td>
+                    <td>
+                      <Text>{item.quantity}</Text>
+                    </td>
+                    <td>
+                      <Text>
+                        {item.is_sale ? <IconCheck color="green" /> : null}
+                      </Text>
+                    </td>
+                    <td>
+                      <Text>
+                        {item.sale_price
+                          ? `$${item.sale_price.toFixed(2)}`
+                          : null}
+                      </Text>
+                    </td>
+                    <td>
+                      <Text>{item.category_id}</Text>
+                    </td>
+                    <td>
+                      <Flex>
+                        <ActionIcon
+                          color="yellow"
+                          onClick={() => {
+                            setEditOpened(true);
+                            console.log(item.id);
+                          }}
+                        >
+                          <IconEdit />
+                        </ActionIcon>
+                        <ActionIcon
+                          ml={10}
+                          color="red"
+                          onClick={(e) => handleRemoveProduct(e, item.id)}
+                        >
+                          <IconTrash />
+                        </ActionIcon>
+                      </Flex>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          {/* </ScrollArea> */}
         </div>
         <Center mt={40}>
-          <Pagination total={10} />
+          <Pagination
+            total={Math.round(products.length / 6)}
+            page={page}
+            onChange={handleChangePage}
+          />
         </Center>
-        <Modal
-          opened={editOpened}
-          onClose={() => setEditOpened(false)}
-          title="Edit product"
-          centered
-          overlayColor={
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[9]
-              : theme.colors.gray[2]
-          }
-          overlayOpacity={0.55}
-          overlayBlur={3}
-        >
-          {/* Modal content */}
-          <EditProduct handleEditProduct={(e) => handleEditProduct(e)} />
-        </Modal>
+        {/* <EditProduct handleEditProduct={(e) => handleEditProduct(e)} /> */}
       </div>
     </div>
   );
